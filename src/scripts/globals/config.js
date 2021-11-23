84fd092fafa0cb1e184726447a46d331cf7f81ee
Syifa Tazkiy Fauziah <36734401+Syifatf@users.noreply.github.com>
2021-11-23 21:19:15 +0700

    Update config.js

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   KEY: 'YOUR_API_KEY',
-  BASE_URL: 'https://restaurant-api.dicoding.dev/',
+  BASE_URL: 'https://restaurant-api.dicoding.dev',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/',
   DEFAULT_LANGUAGE: 'en-us',
   CACHE_NAME: 'Restaurants-V1',
